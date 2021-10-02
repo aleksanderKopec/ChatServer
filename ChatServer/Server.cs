663c04b57bf74d5d5a18f74e70864e0b78a39a0b
aleksanderKopec <a.kopec.b@gmail.com>
2021-10-02 22:22:24 +0200

    Added asynchronous functionality
    Server `should` asynchronously accept connections
    and broadcast messages now.

--- ChatServer/Server.cs
+++ ChatServer/Server.cs
@@ -19,7 +19,7 @@ namespace ChatServer
             IPHostEntry ipHost = Dns.GetHostEntry("127.0.0.1");
             IPAddress ipAddress = ipHost.AddressList[0];
             IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 12345);
-            activateListener(ipAddress, ipEndPoint);
+            activateAsyncListener(ipAddress, ipEndPoint);
         }
 
         private void activateListener(IPAddress ip, IPEndPoint endPoint)
@@ -63,6 +63,97 @@ namespace ChatServer
             catch (Exception e)
             {
                 Console.WriteLine(e);
+            }
+        }
+
+        private void activateAsyncListener(IPAddress ip, IPEndPoint endPoint)
+        {
+            Socket listener = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(endPoint);
+                listener.Listen();
+
+                while (true)
+                {
+                    //accepting the connections
+                    Console.WriteLine("Waiting for a connection");
+                    Socket newClientConnection = listener.Accept();
+
+
+                    //adding the connection to connections list and starting the message transfer
+                    Task.Run(() => transferMessages(newClientConnection));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Caught Exception: {e}");
+                Console.WriteLine("Terminating listener");
+                return;
+            }
+        }
+
+
+        //private void acceptConnections(Socket listener)
+        //{
+        //    while (true)
+        //    {
+        //        //accepting the connections
+        //        Console.WriteLine("Waiting for a connection");
+        //        Socket newClientConnection = listener.Accept();
+
+
+        //        //adding the connection to connections list and starting the message transfer
+        //        Task.Run(() => transferMessages(newClientConnection));
+        //    }
+        //}
+
+        //private void addNewConnection(Socket newConnection)
+        //{
+        //    this.clientsList.Add(newConnection);
+        //}
+
+        private void transferMessages(Socket connection)
+        {
+            while (true)
+            {
+                try
+                {
+                    //Receive message
+                    byte[] receivedMessageEncoded = new byte[1024];
+                    connection.Receive(receivedMessageEncoded);
+
+                    //Decode it and print, this is for debug only
+                    string receivedMessageDecoded = Encoding.ASCII.GetString(receivedMessageEncoded);
+                    Console.WriteLine($"Received message: {receivedMessageDecoded}");
+
+                    //Send it to all other clients
+
+                    for (int i = 0; i < clientsList.Count; i++)
+                    {
+                        if (clientsList[i] == connection) { continue; }
+
+                        clientsList[i].Send(receivedMessageEncoded);
+                    }
+                }
+                //Client ended connection (either knowingly or by some connection issue)
+                catch ( SocketException se)
+                {
+                    Console.WriteLine($"Encountered SocketException: {se}");
+                    Console.WriteLine("Socket timed out, severing the connection");
+                    connection.Close();
+                    return;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Encountered unintended exception: {e}");
+                    Console.WriteLine("Severing the connection");
+                    return;
+                }
+                
+
+
             }
         }
 
